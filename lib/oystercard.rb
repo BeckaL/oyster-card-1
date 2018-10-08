1772e9c83a415e56a86cc4af04f01b9c6e65a74e
jebax <joel.eliot.baxter@gmail.com>
2018-10-08 14:44:16 +0100

    added max card limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,5 @@
 class Oystercard
+  LIMIT = 90
 
   attr_reader :balance
 
@@ -7,6 +8,7 @@ class Oystercard
   end
 
   def top_up(amount)
+    raise "Card limit (£#{LIMIT}) exceeded!" if balance + amount > LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,9 +4,13 @@ describe Oystercard do
 
   it { expect(subject.balance).to eq 0 }
 
-  it 'should top up by a specified amount' do
-    subject.top_up(10)
-    expect(subject.balance).to eq 10
+  it 'should top up balance by a specified amount' do
+    expect { subject.top_up 10 }.to change { subject.balance }.by 10
   end
 
+  it 'should not be able to top up beyond the maximum card limit' do
+    limit = described_class::LIMIT
+    message = "Card limit (£#{limit}) exceeded!"
+    expect { subject.top_up limit + 1 }.to raise_error message
+  end
 end
