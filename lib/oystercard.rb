6803daee04cc4283c15878d4a844ac068b55332e
Becka <rebdok@hotmail.com>
2018-10-08 14:59:06 +0100

    added functionality to touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,11 +5,28 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
     raise "Card limit (£#{LIMIT}) exceeded!" if balance + amount > LIMIT
     @balance += amount
+  end
+
+  def deduct_fare(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,4 +13,25 @@ describe Oystercard do
     message = "Card limit (£#{limit}) exceeded!"
     expect { subject.top_up limit + 1 }.to raise_error message
   end
+
+  it 'deducts fare from balance' do
+    subject.top_up(10)
+    expect { subject.deduct_fare(3) }.to change { subject.balance }.by -3
+  end
+
+  it 'should not be in journey when created' do
+    expect(subject).not_to be_in_journey
+  end
+
+  it 'touch in means card is in journey' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it 'touching out means card is not in journey' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+
 end
